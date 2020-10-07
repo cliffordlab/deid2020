ecd80656b8e1f8a1d6603fdaee6cf2bfa024c892
Zahara Wang <zaharawang@ZaharadeMBP.attlocal.net>
2020-10-07 12:50:33 -0400

    add check dateyear

--- python/deid_Wang.py
+++ python/deid_Wang.py
@@ -6,6 +6,91 @@ phone_pattern ='(\d{3}[-\.\s/]??\d{3}[-\.\s/]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s/]??
 ph_reg = re.compile(phone_pattern)
 
 
+# hospital_file = open("/deid2020/lists/stripped_hospitals.txt")
+# hospotal_list = [line.rstrip('\n') for line in hospital_file]
+# for hospital in hospotal_list:
+#     hospital.rstrip()
+#     alias1 = hospital.replace('&','').replace('\'','').replace('-','').rstrip()
+#     if alias1 not in hospotal_list:
+#         hospotal_list.append(alias1)
+#     if 'hospital' in hospital.lower():
+#         alias = hospital.lower().replace('hospital','').rstrip()
+#         alias2 = alias.lower().replace('&','').replace('\'','').replace('-','').rstrip()
+#         hospotal_list.append(alias)
+#         if alias2 not in hospotal_list:
+#             hospotal_list.append(alias2)
+
+
+# def check_hospital(patient,note,chunk, output_handle):
+#     output_handle.write('Patient {}\tNote {}\n'.format(patient,note))
+#     new_chunk = chunk.lower()
+    
+#     for hospital in hospotal_list:
+#         index = new_chunk.find(hospital.lower())
+#         if index == -1:
+#             continue
+#         result = chunk[index:len(hospital)]
+#         output_handle.write(result+'\n')
+
+def check_dateyear(patient,note,chunk, output_handle):
+    """
+    Inputs:
+        - patient: Patient Number, will be printed in each occurance of personal information found
+        - note: Note Number, will be printed in each occurance of personal information found
+        - chunk: one whole record of a patient
+        - output_handle: an opened file handle. The results will be written to this file.
+            to avoid the time intensive operation of opening and closing the file multiple times
+            during the de-identification process, the file is opened beforehand and the handle is passed
+            to this function. 
+    Logic:
+        Search the entire chunk for phone number occurances. Find the location of these occurances 
+        relative to the start of the chunk, and output these to the output_handle file. 
+        If there are no occurances, only output Patient X Note Y (X and Y are passed in as inputs) in one line.
+        Use the precompiled regular expression to find phones.
+    """
+    # The perl code handles texts a bit differently, 
+    # we found that adding this offset to start and end positions would produce the same results
+    offset = 27
+    # For each new note, the first line should be Patient X Note Y and then all the personal information positions
+    output_handle.write('Patient {}\tNote {}\n'.format(patient,note))
+    #split the record into words 
+    new_chunk = chunk.lower().split(' ')
+    #strip off the ending whitespaces
+    new_chunk = [x.rstrip() for x in new_chunk]
+
+    #go through each word in the record, to see if it is an integer
+    for word in new_chunk:
+        word = word.rstrip()
+        #if the length of the word is bigger than 2 (to avoid index error), get rid of the starting and ending symbols
+        if len(word)>=2:
+            if word[-1] is '.' or word[-1] is ';' or word[-1] is ',' or word[-1] is ')':
+                word = word[:-1]
+            if word[0] is '\'' or word[0] is '(':
+                word = word[1:]   
+        
+        #if the cleaned word is an integer, then if it between 1900-2020 or 90-100, consider them as potentially date year
+        if word.isdigit():
+            word_int = int(word)
+            if  (word_int >=1900 and word_int <=2020) or (word_int<100 and word_int > 90):
+                start_index = str(chunk.lower().find(word)-offset)
+                end_index = str(chunk.lower().find(word)+len(word)-offset)
+                # create the string that we want to write to file ('start start end') 
+                result =start_index +' '+start_index +' '+end_index
+                # write the result to one line of output
+                output_handle.write(result+'\n')
+            
+            # if word_int <10 and word[0] is not '0':
+            #     continue
+            
+            # if word_int >=10 and word_int<=60:
+            #     continue
+            
+            
+
+        
+
+
+
 def check_for_phone(patient,note,chunk, output_handle):
     """
     Inputs:
@@ -97,7 +182,7 @@ def deid_phone(text_path= 'id.text', output_path = 'phone.phi'):
                     # Now we have a full patient note stored in `chunk`, along with patient numerb and note number
                     # pass all to check_for_phone to find any phone numbers in note.
                     check_for_phone(patient,note,chunk.strip(), output_file)
-                    
+                    check_dateyear(patient,note,chunk.strip(), output_file)
                     # initialize the chunk for the next note to be read
                     chunk = ''
                 
